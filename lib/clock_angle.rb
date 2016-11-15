f972d7869db9d506796aeb4b39f6f41d986282cf
Hal Fairless and Jonathan Thom <hal@>
2016-11-14 16:19:16 -0800

    Will show difference between hands

--- lib/clock_angle.rb
+++ lib/clock_angle.rb
@@ -3,9 +3,13 @@ class String
     hours_min = self.split(":")
     minute_hand = hours_min[1].to_f.*(6)
     hour_hand = hours_min[0].to_f.*(30) + hours_min[1].to_f*(0.5)
+    difference = (hour_hand.-(minute_hand)).abs
+
     final_time = []
     final_time.push(hour_hand)
     final_time.push(minute_hand)
+    final_time.push(difference)
+
     final_time
   end
 end
--- spec/clock_angle_spec.rb
+++ spec/clock_angle_spec.rb
@@ -3,10 +3,14 @@ require('clock_angle')
 
 describe('String#clock_angle') do
   it('Will return angle of minute hand based on time (6 degrees per minute)') do
-    expect("1:30".clock_angle()).to(eq([45.0, 180.0]))
+    expect("1:30".clock_angle()).to(eq([45.0, 180.0, 135.0]))
   end
 
   it('Will return angle of hour hand based on time (30 degrees per hour, .5 degrees per minute)') do
-    expect("1:30".clock_angle()).to(eq([45.0, 180.0]))
+    expect("1:30".clock_angle()).to(eq([45.0, 180.0, 135.0]))
+  end
+
+  it('Will calculate the difference in angle between the hour and minute hands and output that value') do
+    expect("1:30".clock_angle()).to(eq([45.0, 180.0, 135.0]))
   end
 end
