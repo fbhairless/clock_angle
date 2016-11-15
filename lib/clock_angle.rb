31bafe7c13a87d3731e8480ae3adabf071e87e8b
Hal Fairless and Jonathan Thom <jonathan@>
2016-11-14 16:16:07 -0800

    Will calculate position of hour hand

--- lib/clock_angle.rb
+++ lib/clock_angle.rb
@@ -1,6 +1,11 @@
 class String
   define_method(:clock_angle) do
     hours_min = self.split(":")
-    hours_min[1].to_i.*(6)
+    minute_hand = hours_min[1].to_f.*(6)
+    hour_hand = hours_min[0].to_f.*(30) + hours_min[1].to_f*(0.5)
+    final_time = []
+    final_time.push(hour_hand)
+    final_time.push(minute_hand)
+    final_time
   end
 end
--- spec/clock_angle_spec.rb
+++ spec/clock_angle_spec.rb
@@ -3,6 +3,10 @@ require('clock_angle')
 
 describe('String#clock_angle') do
   it('Will return angle of minute hand based on time (6 degrees per minute)') do
-    expect("1:30".clock_angle()).to(eq(180))
+    expect("1:30".clock_angle()).to(eq([45.0, 180.0]))
+  end
+
+  it('Will return angle of hour hand based on time (30 degrees per hour, .5 degrees per minute)') do
+    expect("1:30".clock_angle()).to(eq([45.0, 180.0]))
   end
 end
